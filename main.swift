647af114c4def22101282e3afa689cd9612e95fd
Pedro Henrique <83087869+PeedroHenriquee@users.noreply.github.com>
2023-03-26 13:49:12 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+//Faça um programa que calcula a área de um triângulo. Sabe-se que: Área = (base * altura) / 2
+
+let base : Float = 15.30
+let altura : Float = 1.98
+
+let Area = (base * altura) / 2
+
+print("a area e de ", Area, "H" )
